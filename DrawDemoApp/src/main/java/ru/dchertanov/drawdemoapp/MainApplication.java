11e266cd8ff365581c98a8bbf2bec45d303dbb7d
unknown <dachertanov@edu.hse.ru>
2021-09-22 23:26:44 +0300

    Updated maven-shade-plugin

--- DrawDemoApp/src/main/java/ru/dchertanov/drawdemoapp/MainApplication.java
+++ DrawDemoApp/src/main/java/ru/dchertanov/drawdemoapp/MainApplication.java
@@ -12,7 +12,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1400, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("DrawDemoApp");
         stage.setScene(scene);
         stage.setResizable(false);
