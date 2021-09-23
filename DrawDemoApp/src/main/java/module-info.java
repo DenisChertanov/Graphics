8ee321219ab68dcd457381799b55f15b21436be2
Denis <dachertanov@edu.hse.ru>
2021-09-23 18:40:57 +0300

    Added module-info

---
+++ DrawDemoApp/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module primitives_demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.swing;
+
+    opens ru.dchertanov.drawdemoapp.algo to javafx.fxml;
+    opens ru.dchertanov.drawdemoapp to javafx.fxml;
+    opens ru.dchertanov.drawdemoapp.controller to javafx.fxml;
+
+    exports ru.dchertanov.drawdemoapp;
+    exports ru.dchertanov.drawdemoapp.controller;
+    exports ru.dchertanov.drawdemoapp.algo;
+}
