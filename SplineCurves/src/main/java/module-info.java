bb0d95ac84e40920c23b7fc3cc8879fc522c252a
Denis Chertanov <dachertanov@Deniss-MacBook-Air.local>
2021-11-06 17:35:10 +0300

    Inittialize SlineCurve project

---
+++ SplineCurves/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module ru.dchertanov.fillandclippingdemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.swing;
+
+//    opens ru.dchertanov.splinecurves.controller to javafx.fxml;
+
+    exports ru.dchertanov.splinecurves;
+//    exports ru.dchertanov.splinecurves.controller;
+//    exports ru.dchertanov.splinecurves.figures;
+//    exports ru.dchertanov.splinecurves.util;
+}
